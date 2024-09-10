8544a8ffa039facdefecc383a118d4f992df0346
Christian Westgaard <comlock@gmail.com>
2024-09-10 14:04:33 +0200

    Remove unused code

--- src/main/resources/lib/enonic/static/runMode.ts
+++ src/main/resources/lib/enonic/static/runMode.ts
@@ -1,7 +1,4 @@
 const helper = __.newBean<{
   isDevMode: () => boolean
-  getFingerprint: (application: string) => string
 }>('lib.enonic.libStatic.AppHelper');
 export const isDev = () => helper.isDevMode();
-
-export const getFingerprint = (application: string) => helper.getFingerprint(application);
