94102c3b6cb9bd03524514ee3937929929df467e
Christian Westgaard <comlock@gmail.com>
2024-09-16 14:54:41 +0200

    Fix #386 Remove buildGetter and get functions

--- src/main/resources/lib/enonic/static/index.ts
+++ src/main/resources/lib/enonic/static/index.ts
@@ -1,11 +1,4 @@
 export {RESPONSE_CACHE_CONTROL} from '/lib/enonic/static/constants';
 
-export { buildGetter } from '/lib/enonic/static/buildGetter';
-export { get } from '/lib/enonic/static/get';
 export { requestHandler } from '/lib/enonic/static/service/requestHandler';
 export { webAppCacheControl } from '/lib/enonic/static/service/webAppCacheControl';
-
-
-// Exported for testing only:
-export { getPathError as __getPathError__ } from '/lib/enonic/static/path/getPathError';
-export { resolveRoot as __resolveRoot__ } from '/lib/enonic/static/path/resolveRoot';
--- src/main/resources/lib/enonic/static/options/verifyEtagOption.ts
+++
@@ -1,5 +0,0 @@
-export const verifyEtagOption = (etag:Boolean|undefined) => {
-  if (etag !== true && etag !== false && etag !== undefined) {
-    throw Error("Unexpected 'etag' option: only true, false or undefined are allowed.");
-  }
-};
