c0dd6877fca65e1baa1d01a8d2a2aa9e06b3495c
Christian Westgaard <comlock@gmail.com>
2024-09-24 13:32:48 +0200

    Fix #404 Implement spaNotFoundHandler

--- src/main/resources/lib/enonic/static/index.ts
+++ src/main/resources/lib/enonic/static/index.ts
@@ -1,4 +1,5 @@
 export {RESPONSE_CACHE_CONTROL} from '/lib/enonic/static/constants';
 
 export {requestHandler} from '/lib/enonic/static/service/requestHandler';
+export {spaNotFoundHandler} from '/lib/enonic/static/service/spaNotFoundHandler';
 export {webAppCacheControl} from '/lib/enonic/static/service/webAppCacheControl';
