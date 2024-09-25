f42de67fbf3a73fae533573b78fda04bebcee67f
Christian Westgaard <comlock@gmail.com>
2024-09-25 12:48:31 +0200

    warn

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,7 +5,7 @@ import { dirname } from 'path';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const LEVEL = 'off';
+const LEVEL = 'warn';
 
 export default [
   ...enonicConfig,
